ccb89db00ee10bb39074c2bbeb7b0fe41df7cd01
Jeraemius Shannon J <94852977+jeraemiusshannonj@users.noreply.github.com>
2023-02-26 19:35:00 +0530

    Create imports.py

---
+++ imports.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+import keras
+from keras.models import Sequential
+from keras.layers import Dense,Dropout
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+np.random.seed(0)
